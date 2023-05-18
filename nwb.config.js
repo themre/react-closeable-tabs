5a817d2996f0c15252c5a6328ef309df531cbe06
Octoping <mooner925@gmail.com>
2023-05-18 20:07:51 +0900

    feat: add typescript config to nwb

--- nwb.config.js
+++ nwb.config.js
@@ -8,5 +8,22 @@ module.exports = {
         react: 'React'
       }
     }
-  }
+  },
+  webpack: {
+    extra: {
+      entry: {
+        index: './src/index.tsx',
+        demo: './demo/src/index.tsx',
+      },
+      output: {
+        filename: '[name].js',
+      },
+      resolve: {
+        extensions: ['.ts', '.tsx', '.js', '.jsx'],
+      },
+      module: {
+        rules: [{test: /\.tsx$/, loader: 'ts-loader'}],
+      },
+    },
+  },
 }
